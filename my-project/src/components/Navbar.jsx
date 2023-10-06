c6951f00301242b6b1c4443feee18177ae15a607
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2023-10-06 08:58:47 -0500

    Update-pre-Firebase

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className = 'rounded-div flex items-center justify-between h-20 font-bold'>
         <Link to='/'>
-            <h1 className='text-2xl'>Crypto EX</h1>
+            <h1 className='text-2xl'>Crypto AMO</h1>
         </Link>
         <div className='hidden md:block'>
             <ThemeToggle />
